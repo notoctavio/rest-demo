dbc977d9ba7bd5111b416116d961f08482f97ceb
notoctavio <danielvizaru@gmail.com>
2025-03-15 17:48:46 +0200

    first commit

---
+++ src/main/java/com/octavio/rest_demo/controller/CloudVendorAPIService.java
@@ -0,0 +1,45 @@
+package com.octavio.rest_demo.controller;
+
+import com.octavio.rest_demo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId) {
+        return cloudVendor;
+
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId) {
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+
+    }
+
+
+
+
+
+
+
+}
